c1bc30cfc938a8651aa7e6d4afc8c9437aef0aed
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-20 10:13:35 +0530

    Update logo.blade.php

--- resources/views/pip/logo.blade.php
+++ resources/views/pip/logo.blade.php
@@ -1,6 +1,6 @@
 <div class="pip-logo">
     <a class="pip-logo-link" href="/">
-        <img src="/pip/assets/img/jata_negara_20x20.png" alt="Portal Industri Pertahanan" />
+        <img src="/pip/assets/img/logo-pip.png" alt="Portal Industri Pertahanan" />
         Portal Industri Pertahanan<br />
         Kementerian Pertahanan Malaysia<br />
     </a>
