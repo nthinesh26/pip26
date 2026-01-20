6ea52b9ac15bc57767ad763ac55d28328d6510e8
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-20 14:43:36 +0530

    Update password-rest-email.blade.php

--- resources/views/email/password-rest-email.blade.php
+++ resources/views/email/password-rest-email.blade.php
@@ -137,10 +137,6 @@
 
 
                                     <p style="margin:0 0 12px 0;">Thank you.</p>
-
-                                    <p style="margin:12px 0 0 0;">
-                                        <p> <img src="{{ $url }}/pip/assets/img/footer-logo-portal.png" /> </p>
-                                    </p>
                                 </div>
                             </td>
                         </tr>
