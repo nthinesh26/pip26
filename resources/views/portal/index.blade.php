a2979f82596f1a12e7d68109b7e2631f1d05c1ed
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-20 06:15:49 +0530

    Update index.blade.php

--- resources/views/portal/index.blade.php
+++ resources/views/portal/index.blade.php
@@ -276,7 +276,7 @@
             </div>
             <div class="pip-footer-links">
                 <a class="pip-footer-link" href="https://myip.mod.gov.my/polisi-privasi"><span data-i18n="footer_privacy">POLISI PRIVASI</span></a>
-                {{-- <a class="pip-footer-link" href="[[TERMS_URL]]"><span data-i18n="footer_terms">TERMA PENGGUNAAN</span></a> --}}
+                <a class="pip-footer-link" href="javascript:void(0)"><span data-i18n="footer_terms">TERMA PENGGUNAAN</span></a>
                 <a class="pip-footer-link" href="https://myip.mod.gov.my/hubungi-kami" target="_blank" data-i18n="nav_contact">HUBUNGI KAMI</a>
             </div>
         </div>
