6e962d5a42eee4de4a312ff9e92b35a930492bd4
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-20 10:18:16 +0530

    Update p2-sc-script.blade.php

--- resources/views/pages/local-company/sub/p2-sc-script.blade.php
+++ resources/views/pages/local-company/sub/p2-sc-script.blade.php
@@ -12,7 +12,7 @@
 
         $("#btn_add_director").click(function(e) {
             let status = $("input[name='director_status']:checked").val();
-            let valid = $("input[name='director_status']")prop('checked');
+            let valid = $("input[name='director_status']").prop('checked');
 
             if($("#director_name").val() != '' &&  $("#director_id_passport").val() != '' && $("#director_nationality").val() != '' && $("#director_position").val() != '' && $("#director_shareholding_pct").val() != '' && valid){
                 $.ajax({
