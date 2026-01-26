006e55e8fa6d6f1988f03e06a047a92c303197a5
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-26 17:30:31 +0530

    Update dir-res.blade.php

--- resources/views/portal/dir-res.blade.php
+++ resources/views/portal/dir-res.blade.php
@@ -5,7 +5,7 @@
                 $users = User::whereIn('type', $type)->where('status', '<>', 'del')->get();
             } else {
                 $users = User::whereIn('type', ['local', 'royal', 'institute', 'oem'])
-                    ->where('status', '<>' 'del')
+                    ->where('status', '<>', 'del')
                     ->get();
             }
         @endphp
