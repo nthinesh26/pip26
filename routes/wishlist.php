b736a859640db6791f6f7de57a3a5796aa171de4
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-26 18:46:04 +0530

    c

--- routes/wishlist.php
+++ routes/wishlist.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\WishListController;
 
 Route::middleware(['auth'])->group(function () {
-	Route::get('/pip/profile/create-wishlist', [WishListController::class, 'wishListWindow']);
+
     Route::get('/pip/profile/create-wishlist/{wish_list}', [WishListController::class, 'wishListView']);
     Route::get('/pip/profile/view-wishlist/{wish_list}', [WishListController::class, 'wishListView']);
    Route::get('/pip/wishilist-create/confirmation', [WishListController::class, 'confirmWL']);
