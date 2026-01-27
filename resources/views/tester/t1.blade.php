7b465bfeee3409e70c3017a431da70b2a9d665f7
N. Thinesh <narasingam.thinesh@scicom.com.my>
2026-01-27 15:30:57 +0530

    Update t1.blade.php

--- resources/views/tester/t1.blade.php
+++ resources/views/tester/t1.blade.php
@@ -7,6 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="container-fluid">
-    <iframe src="https://staging-myip.boustead.com.my" width="1000" heigh="700" style="border: 0px !important"></iframe>
+    <iframe src="https://staging-myip.boustead.com.my" width="1000" heigh="1000" style="border: 0px !important"></iframe>
 </body>
 </html>
